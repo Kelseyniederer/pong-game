9e8558f5bceb493d20f5521302e35799a379bbc3
Kelsey Niederer <kelseyniederer12@gmail.com>
2021-08-03 22:34:26 -0500

    the ball bounces around in frame

--- pong.js
+++ pong.js
@@ -26,8 +26,36 @@ const ball = new Ball;
 ball.position.x = 100;
 ball.position.y = 50;
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+ball.velocity.x = 100;
+ball.velocity.y = 100;
 
-context.fillStyle = '#fff';
-context.fillRect(ball.position.x, ball.position.y, ball.size.x, ball.size.y);
+let lastTime;
+function callback(millis) {
+    if (lastTime) {
+        update((millis - lastTime) / 1000);
+    }
+
+    lastTime = millis;
+    requestAnimationFrame(callback);
+}
+
+function update(dt) { 
+    ball.position.x += ball.velocity.x * dt;
+    ball.position.y += ball.velocity.y * dt;
+
+    if (ball.position.x < 0 || ball.position.x > canvas.width){
+        ball.velocity.x = -ball.velocity.x;
+    }
+
+    if (ball.position.y < 0 || ball.position.y > canvas.height){
+        ball.velocity.y = -ball.velocity.y;
+    }
+
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = '#fff';
+    context.fillRect(ball.position.x, ball.position.y, ball.size.x, ball.size.y);
+}
+
+callback();
