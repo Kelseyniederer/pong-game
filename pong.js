295bdbcbc011adf6aa27755b0315aca9fb0589bb
Kelsey Niederer <kelseyniederer12@gmail.com>
2021-08-03 21:52:00 -0500

    added pong.html and pong.js

---
+++ pong.js
@@ -0,0 +1,15 @@
+class Vector {
+    constructor(x = 0, y = 0) {
+        this.x = x; 
+        this.y = y;
+    }
+}
+
+const canvas = document.getElementById('pong');
+const context = canvas.getContext('2d');
+
+context.fillStyle = '#000';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+context.fillStyle = '#fff';
+context.fillRect(0, 0, 10, 10);
